0574d0698a5c03929231101a9a9c59a210ad0887
KRKDEV <111200477+KRKDEV@users.noreply.github.com>
2023-05-24 21:31:53 +0200

    Added Home & Movies page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
 import NavBar from './NavBar/NavBar';
+import HomePage from 'pages/HomePage/HomePage';
+import MoviesPage from 'pages/MoviesPage/MoviesPage';
 export const App = () => {
   return (
-    <div>
+    <>
       <NavBar />
-    </div>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/movies" element={<MoviesPage />}></Route>
+      </Routes>
+    </>
   );
 };
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,11 +1,26 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+
 import css from './NavBar.module.css';
 const NavBar = () => {
+  const location = useLocation();
   return (
     <header>
       <div className={css['navbar__container']}>
-        <Link to="/" className={css['navbar__link']}>
+        <Link
+          to="/"
+          className={
+            location.pathname === '/' ? css.active : css['navbar__link']
+          }
+        >
           Home
+        </Link>
+        <Link
+          to="/movies"
+          className={
+            location.pathname === '/movies' ? css.active : css['navbar__link']
+          }
+        >
+          Movies
         </Link>
       </div>
     </header>
